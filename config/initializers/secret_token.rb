078b88320261f0fd28612149cb632a4ccef9f51c
Timothee Guerin <timothee.guerin@outlook.com>
2013-10-08 17:59:11 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mathlearner::Application.config.secret_key_base = 'ef4d38d4ca909034cd2b653eece9fe7a72e33fcbb0d49709442dfcede221b834fdd31deb936efb1f0409a118af673028243688ee37c309f7ffd1671ac99e157c'

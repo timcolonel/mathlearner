b1d576b0801e8dd44858e5ebb558f98b9e472cb4
Timothee Guerin <timothee.guerin@outlook.com>
2013-10-08 18:22:09 -0400

    boostrap, rails_admin, devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Mathlearner::Application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
   devise_for :users
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your routes lay out with 'rake routes'.
 
-  # You can have the root of your site routed with "root"
+  # You can have the root of your site routed with 'root'
   # root 'welcome#index'
 
   # Example of regular route:
